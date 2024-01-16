481855a6498fe3f41511cd234b761d87453110f6
Boris Stefanov <bobisfe@gmail.com>
2024-01-16 02:24:14 +0200

    Finished the "new type" page front end implementation, backend is next.

---
+++ app/dashboard/newType/components/newFieldComponent.tsx
@@ -0,0 +1,58 @@
+import { FormControl, IconButton, InputLabel, MenuItem, Select, SelectChangeEvent, TableCell, TableRow, TextField } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import styles from "../newFieldComponent.module.css";
+import React from "react";
+
+export type FieldObject = {
+    fieldName: string;
+    fieldType: string;
+}
+
+type NewFieldComponentProps = {
+    index: number;
+    onDelete: () => void;
+    fields: FieldObject[];
+    setFields: React.Dispatch<React.SetStateAction<FieldObject[]>>;
+}
+
+export default function NewFieldComponent(props: NewFieldComponentProps) {
+
+    const handleTypeChange = (event: SelectChangeEvent) => {
+        const newFields = [...props.fields];
+        newFields[props.index].fieldType = event.target.value as string;
+        props.setFields(newFields);
+    };
+
+    const handleFieldNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newFields = [...props.fields];
+        newFields[props.index].fieldName = event.target.value;
+        props.setFields(newFields);
+    }
+
+    return (
+        <TableRow>
+            <TableCell width={"50%"} style={{ borderRight: '1px solid #515151' }}>
+                <TextField value={props.fields[props.index].fieldName} onChange={handleFieldNameChange} required 
+                    fullWidth label="Field name" variant="outlined" />
+            </TableCell>
+            <TableCell width={"50%"}>
+                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+                    <FormControl fullWidth required>
+                      <InputLabel>Type</InputLabel>
+                      <Select label="Type" value={props.fields[props.index].fieldType} onChange={handleTypeChange} >
+                        <MenuItem value={"string"}>String</MenuItem>
+                        <MenuItem value={"number"}>Number</MenuItem>
+                        <MenuItem value={"date"}>Date</MenuItem>
+                        <MenuItem value={"linkedObject"}>LinkedObject</MenuItem>
+                        <MenuItem value={"hrefObject"}>HrefObject</MenuItem>
+                        <MenuItem value={"numberWithSuffix"}>NumberWithSuffix</MenuItem>
+                      </Select>
+                    </FormControl>
+                    <IconButton style={{color:"#c2ffaf", marginLeft: "10px"}} onClick={props.onDelete} aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                </div>
+            </TableCell>
+        </TableRow>
+    );
+}
--- app/dashboard/newType/page.tsx
+++ app/dashboard/newType/page.tsx
@@ -1,7 +1,92 @@
-export default function NewTypePage() {
+"use client";
+import { Button, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
+import styles from './page.module.css';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import NewFieldComponent, { FieldObject } from './components/newFieldComponent';
+import { useState } from 'react';
+
+export default function NewTypePage() 
+{
+    const [fields, setFields] = useState<FieldObject[]>([]);
+    const [typeName, setTypeName] = useState<string>("");
+
+    function handleTypeNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setTypeName(event.target.value);
+    }
+
+    const addField = () => {
+        setFields(prevFields => [...prevFields, { fieldName: "", fieldType: "" }]);
+    }
+
+    function deleteField(index: number) {
+        setFields(prevFields => {
+            const updatedFields = [...prevFields];
+            updatedFields.splice(index, 1);
+            return updatedFields;
+        });
+    }
+
+    function validateFieldsAndSend() {
+        var validCheck = true;
+        
+        if (typeName === "") {
+            validCheck = false;
+        }
+        for (let i = 0; i < fields.length && validCheck; i++) 
+        {
+            if (fields[i].fieldName === "" || fields[i].fieldType === "") {
+                validCheck = false;
+            }
+        }
+        console.log(validCheck);
+        console.log(fields);
+    }
+    
     return (
-        <main>
-            <h1>New type page</h1>
+        <main className={styles.NewTypePage}>
+            <div className={styles.EnterTypeName}>
+                <h2>Create new type:</h2>
+                <TextField className={styles.EnterTypeNameLabel} id="type-name-field" value={typeName} 
+                    onChange={handleTypeNameChange} required label="Enter type name:" variant="outlined" />
+            </div>
+
+            <div className={styles.typeTable}>
+                <TableContainer component={Paper} style={{ background: '#00000000' }}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell width={"50%"} style={{ borderRight: '1px solid #515151', 
+                                    fontWeight: 700, fontSize: "20px", color: "#c2ffaf" }}>
+                                    Field name</TableCell>
+                                <TableCell className={styles.TitleCell} width={"50%"} style={{
+                                    fontWeight: 700, fontSize: "20px", color: "#c2ffaf"}}>
+                                    Field type</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                fields.map((e, i) => 
+                                <NewFieldComponent key={i} index={i} setFields={setFields} 
+                                fields={fields} onDelete={()=>deleteField(i)}/>)
+                            }
+                        </TableBody>
+                    </Table>
+                    <div className={styles.TableAddButton}>
+                        <IconButton style={{color:"#c2ffaf"}} aria-label="add" onClick={addField}>
+                            <AddCircleOutlineIcon style={{fontSize: "30px"}} />
+                        </IconButton>
+                    </div>
+                </TableContainer>
+            </div>
+
+            <div className={styles.BottomButtonBox}>
+                <Button variant="contained" color="success" href="/dashboard">
+                    Go back
+                </Button>
+                <Button variant="contained" color="success" onClick={validateFieldsAndSend}>
+                    Create
+                </Button>
+            </div>
         </main>
     );
 }
